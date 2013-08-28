3f28a4af368c3ed887b938b340361d0f5a214296
lukerymarz@gmail.com <lukerymarz@gmail.com>
2013-08-28 01:22:14 -0700

    using localStorage a bit

--- public/javascripts/steamnode.js
+++ public/javascripts/steamnode.js
@@ -1,3 +1,5 @@
+var stopProcessing = false;
+
 $( function() {
 	var gameIds = []
 	$.each($('.games tr'), function() {
@@ -6,9 +8,10 @@ $( function() {
 	var self = this;
 	getGameAchievements = function() {
 		gameId = gameIds.pop();
-		$.getJSON('/api/global_achievements/' + gameId, function(data) {
-
+		getLocalGameAchievements(gameId, function(data) {
 			var cheevos = data.achievementpercentages.achievements;
+
+			storeGameAchievementsLocally(gameId, cheevos);
 			if(cheevos.length == 0) {
 				// remove irrelevant element
 				$('.games tr.game_' + gameId).remove();
@@ -24,8 +27,57 @@ $( function() {
 		}).fail(function(){
 			console.log('fail')
 		}).always(function(){
-			getGameAchievements();
+			if(!stopProcessing) {
+				getGameAchievements();
+			}
 		});
 	};
-	setTimeout(getGameAchievements, 2000);
+
+	getLocalGameAchievements = function(gameId, fn) {
+		if(haveLocalGameAchievements(gameId))
+		{
+			cheevos = JSON.parse(localStorage["game_" + gameId]);
+			// fake promise
+			pr = {};
+			pr.fail = function() { return pr};
+			pr.always = function(fn) {fn(); return pr};
+			fn({achievementpercentages: {achievements: cheevos}});
+			return pr;
+		}
+		else {
+			return $.getJSON('/api/global_achievements/' + gameId, fn);
+		}
+	}
+
+	haveLocalGameAchievements = function(gameId) {
+		if(typeof(Storage)!=="undefined")
+		{
+			return localStorage["game_" + gameId + "_valid"] == "t";
+		}
+		else {
+			return false;
+		}
+	}
+
+	storeGameAchievementsLocally = function(gameId, achievements) {
+		if(typeof(Storage)!=="undefined")
+		{
+			localStorage["game_" + gameId + "_valid"] = "t";
+			localStorage["game_" + gameId] = JSON.stringify(achievements);
+		}
+	}
+
+	setTimeout(getGameAchievements, 200);
+	$('.stop_processing').click(function() {
+		stopProcessing = !stopProcessing;
+
+		if(!stopProcessing) {
+			$(this).text("Stop Processing");
+			setTimeout(getGameAchievements, 200);
+		}
+		else {
+			$(this).text("Start Processing");
+		}
+
+	});
 })
